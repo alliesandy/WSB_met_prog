d29c70b803e6a27076dc7a4b509a261b6ea234c0
Aleksandra Jarek <olajarek@wp.pl>
2022-10-04 21:15:36 +0200

    adding body of main fnction

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include "kwadrat.hpp"
 
 int main(int argc, char* argv[]) {
-    
+        std::cout << argv[0] << std::endl;
+    if (argc < 2) {
+        std::cout << "Nie podano liczby" << std::endl;
+    } 
+    else {
+        std::cout << kwadrat(std::stof(argv[1])) << std::endl;
+    }
 }
